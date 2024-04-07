63712daa38b05dde839d2f021dab8e02224d2a3d
williangama <>
2024-04-07 15:18:14 +0100

    remove unused code

--- scripts/generate_local_properties.sh
+++ scripts/generate_local_properties.sh
@@ -5,10 +5,7 @@ export LOCAL_PROPERTIES
 
 if [ ! -f "$LOCAL_PROPERTIES" ]; then
     touch $LOCAL_PROPERTIES
-#    echo "api_key=$API_KEY" >> $LOCAL_PROPERTIES
-#    echo "gpr_username=$GPR_USERNAME" >> $LOCAL_PROPERTIES
-#    echo "gpr_key=$GPR_KEY" >> $LOCAL_PROPERTIES
-#    echo "sonar_token=$SONAR_TOKEN" >> $LOCAL_PROPERTIES
+
     {
       echo "api_key=$API_KEY"
       echo "gpr_username=$GPR_USERNAME"
