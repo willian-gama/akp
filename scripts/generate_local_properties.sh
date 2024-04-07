f52961e19c6f09adfa48897cf073242d5fc5b156
williangama <>
2024-04-07 12:17:08 +0100

    attempt to simplify config

--- scripts/generate_local_properties.sh
+++ scripts/generate_local_properties.sh
@@ -5,11 +5,17 @@ export LOCAL_PROPERTIES
 
 if [ ! -f "$LOCAL_PROPERTIES" ]; then
     touch $LOCAL_PROPERTIES
-
-    echo "api_key=$API_KEY" >> $LOCAL_PROPERTIES
-    echo "gpr_username=$GPR_USERNAME" >> $LOCAL_PROPERTIES
-    echo "gpr_key=$GPR_KEY" >> $LOCAL_PROPERTIES
-    echo "sonar_token=$SONAR_TOKEN" >> $LOCAL_PROPERTIES
+#    echo "api_key=$API_KEY" >> $LOCAL_PROPERTIES
+#    echo "gpr_username=$GPR_USERNAME" >> $LOCAL_PROPERTIES
+#    echo "gpr_key=$GPR_KEY" >> $LOCAL_PROPERTIES
+#    echo "sonar_token=$SONAR_TOKEN" >> $LOCAL_PROPERTIES
+    {
+      echo "api_key=$API_KEY"
+      echo "gpr_username=$GPR_USERNAME"
+      echo "gpr_key=$GPR_KEY"
+      echo "sonar_token=$SONAR_TOKEN"
+    }  >> $LOCAL_PROPERTIES
 
     cat $LOCAL_PROPERTIES
 fi
+}
