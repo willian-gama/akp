8a1df4f79e9dc1ecbeab0179a73871c216607818
williangama <>
2024-04-07 12:13:28 +0100

    add missing config

--- scripts/generate_local_properties.sh
+++ scripts/generate_local_properties.sh
@@ -9,6 +9,7 @@ if [ ! -f "$LOCAL_PROPERTIES" ]; then
     echo "api_key=$API_KEY" >> $LOCAL_PROPERTIES
     echo "gpr_username=$GPR_USERNAME" >> $LOCAL_PROPERTIES
     echo "gpr_key=$GPR_KEY" >> $LOCAL_PROPERTIES
+    echo "sonar_token=$SONAR_TOKEN" >> $LOCAL_PROPERTIES
 
     cat $LOCAL_PROPERTIES
 fi
