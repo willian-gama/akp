e077f94b9936ffd06a62994870dc529648aacd88
williangama <willian_gama@live.com>
2024-04-11 16:43:04 +0100

    remove duplicate bash script

--- ci/scripts/generate_local_properties.sh
+++ ci/scripts/generate_local_properties.sh
@@ -1,11 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
-PROPERTIES="local.properties"
+LOCAL_PROPERTIES="local.properties"
+export LOCAL_PROPERTIES
 
-touch $PROPERTIES
+if [ ! -f "$LOCAL_PROPERTIES" ]; then
+    touch $LOCAL_PROPERTIES
 
-{
-  echo "api_key=$API_KEY"
-} >> $PROPERTIES
+    {
+      echo "api_key=$API_KEY"
+      echo "gpr_username=$GPR_USERNAME"
+      echo "gpr_key=$GPR_KEY"
+      echo "sonar_token=$SONAR_TOKEN"
+    }  >> $LOCAL_PROPERTIES
 
-cat $PROPERTIES
+    cat $LOCAL_PROPERTIES
+fi
