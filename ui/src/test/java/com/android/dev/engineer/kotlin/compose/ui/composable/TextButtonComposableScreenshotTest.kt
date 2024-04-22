5e3ec47b71ad45a6c1f3d38983e574c0a78f5ace
williangama <willian_gama@live.com>
2024-04-22 13:52:55 +0100

    make screenshot test fails for testing purposes

--- ui/src/test/java/com/android/dev/engineer/kotlin/compose/ui/composable/TextButtonComposableScreenshotTest.kt
+++ ui/src/test/java/com/android/dev/engineer/kotlin/compose/ui/composable/TextButtonComposableScreenshotTest.kt
@@ -20,7 +20,7 @@ class TextButtonComposableScreenshotTest {
         paparazzi.snapshot {
             KotlinComposeAppTheme {
                 TextButtonComposable(
-                    text = "Skip",
+                    text = "Skips",
                     onClick = {}
                 )
             }
