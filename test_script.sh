6acedf66b9ba0689757172d0c9d87f2519ec6c87
williangama <willian_gama@live.com>
2024-05-05 18:36:18 +0100

    add script

---
+++ test_script.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# NOT USED, ONLY FOR TESTING PURPOSES
+
+SPLIT_UNIT_TEST_CLASS_NAMES="feature.upcoming_movies.com.android.dev.engineer.kotlin.compose.feature.upcoming_movies.test.screen.UpcomingMoviesScreenComposableScreenshotTest"
+BUILD_VARIANT=debug
+UNIT_TEST_COMMAND=test${BUILD_VARIANT}UnitTest
+
+run_unit_tests() {
+  COMMAND="$1"
+  echo "Running unit tests: $COMMAND"
+  ./gradlew $COMMAND
+}
+
+group_unit_tests_per_module() {
+  MODULES=$(
+    echo "$SPLIT_UNIT_TEST_CLASS_NAMES" |
+    awk '{
+      for (i=1; i<=NF; i++) {
+        sub(/\.com\..*/, "", $i)
+        print($i)
+      }
+    }' |
+    awk '!visited[$0]++' ORS=" "
+  )
+
+  read -r -a MODULES <<< "$MODULES"
+
+  for MODULE in "${MODULES[@]}"; do
+    UNIT_TEST_CLASS_NAMES=$(
+      echo "$SPLIT_UNIT_TEST_CLASS_NAMES" |
+      awk "/^$MODULE\./" |
+      awk -F "^$MODULE\." '{
+        print("--tests", $2)
+      }' ORS=" "
+    )
+
+    if [ -n "$UNIT_TEST_CLASS_NAMES" ]; then
+      run_unit_tests "${MODULE//\./:}:$UNIT_TEST_COMMAND $UNIT_TEST_CLASS_NAMES"
+    fi
+  done
+}
+
+group_unit_tests_per_module
