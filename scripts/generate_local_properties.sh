dcafa6d343bebb3526b3c97e91dee10211a30032
williangama <>
2024-08-28 11:10:25 +0100

    update local.properties to include sonar vars

--- scripts/generate_local_properties.sh
+++ scripts/generate_local_properties.sh
@@ -4,10 +4,16 @@ LOCAL_PROPERTIES="local.properties"
 touch $LOCAL_PROPERTIES
 
 {
+  # The Movie API
   echo "api_key=$API_KEY"
+  # Github packages
   echo "gpr_username=$GPR_USERNAME"
   echo "gpr_key=$GPR_KEY"
+  # Sonar
   echo "sonar_token=$SONAR_TOKEN"
+  echo "sonar_project_key=$SONAR_PROJECT_KEY"
+  echo "sonar_organization_key=$SONAR_ORGANIZATION_KEY"
+  echo "sonar_project_name=$SONAR_PROJECT_NAME"
 }  >> $LOCAL_PROPERTIES
 
 cat $LOCAL_PROPERTIES
