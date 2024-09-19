be5449037a59af219899a04a930fe31dfc1d9280
williangama <>
2024-09-19 16:47:18 +0100

    cache data

--- data/src/androidTest/java/com/android/dev/engineer/kotlin/compose/data/test/shared_preferences/UserEncryptedSharedPrefsTest.kt
+++ data/src/androidTest/java/com/android/dev/engineer/kotlin/compose/data/test/shared_preferences/UserEncryptedSharedPrefsTest.kt
@@ -1,11 +1,12 @@
 package com.android.dev.engineer.kotlin.compose.data.test.shared_preferences
 
-import android.content.SharedPreferences
 import androidx.core.content.edit
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.android.dev.engineer.kotlin.compose.data.di.SharedPrefsModule.provideEncryptedSharedPreferences
 import com.android.dev.engineer.kotlin.compose.data.shared_preferences.UserEncryptedSharedPrefsImpl
+import io.mockk.spyk
+import io.mockk.verify
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.After
@@ -14,20 +15,21 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
+private const val FAKE_SESSION_ID = "fake_session_id"
+
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 class UserEncryptedSharedPrefsTest {
-    companion object {
-        private const val FAKE_SESSION_ID = "fake_session_id"
-    }
-
-    private lateinit var sharedPreferences: SharedPreferences
     private lateinit var userEncryptedSharedPrefs: UserEncryptedSharedPrefsImpl
+    private var sharedPreferences = spyk(
+        provideEncryptedSharedPreferences(context = ApplicationProvider.getApplicationContext())
+    )
 
     @Before
     fun setUp() {
-        sharedPreferences = provideEncryptedSharedPreferences(context = ApplicationProvider.getApplicationContext())
-        userEncryptedSharedPrefs = UserEncryptedSharedPrefsImpl(sharedPreferences = sharedPreferences)
+        userEncryptedSharedPrefs = UserEncryptedSharedPrefsImpl(
+            sharedPreferences = sharedPreferences
+        )
     }
 
     @After
@@ -36,13 +38,27 @@ class UserEncryptedSharedPrefsTest {
     }
 
     @Test
-    fun testSaveSessionId() = runTest {
+    fun testGetCachedSessionId() = runTest {
         userEncryptedSharedPrefs.saveSessionId(sessionId = FAKE_SESSION_ID)
         assertEquals(FAKE_SESSION_ID, userEncryptedSharedPrefs.getSessionId())
+        verify(exactly = 0) {
+            sharedPreferences.getString(FAKE_SESSION_ID, "")
+        }
     }
 
     @Test
-    fun testClearAll() = runTest {
+    fun testGetSessionIdNotCached() = runTest {
+        userEncryptedSharedPrefs.saveSessionId(sessionId = FAKE_SESSION_ID)
+        repeat(times = 2) {
+            assertEquals(FAKE_SESSION_ID, userEncryptedSharedPrefs.getSessionId())
+        }
+        verify(exactly = 1) {
+            sharedPreferences.getString(FAKE_SESSION_ID, "")
+        }
+    }
+
+    @Test
+    fun testSessionIdIsCleared() = runTest {
         userEncryptedSharedPrefs.saveSessionId(sessionId = FAKE_SESSION_ID)
         userEncryptedSharedPrefs.clearAll()
         assertEquals("", userEncryptedSharedPrefs.getSessionId())
