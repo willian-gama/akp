b63e67cd0cec8bea4725622fd02e88ce6a03fab9
Willian Gama <10974743+willian-gama@users.noreply.github.com>
2024-11-15 22:18:14 +0000

    update AsyncImageComposable.kt

--- ui/src/main/java/com/android/dev/engineer/kotlin/compose/ui/composable/AsyncImageComposable.kt
+++ ui/src/main/java/com/android/dev/engineer/kotlin/compose/ui/composable/AsyncImageComposable.kt
@@ -2,7 +2,7 @@ package com.android.dev.engineer.kotlin.compose.ui.composable
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import coil.compose.AsyncImage
+import coil3.compose.AsyncImage
 
 @Composable
 fun AsyncImageComposable(
